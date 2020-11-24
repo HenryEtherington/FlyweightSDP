e34232789228fa1f79bc384de03649f7f5af57d9
Harry Etherington <43179139+HenryEtherington@users.noreply.github.com>
2020-11-24 13:02:42 +0000

    Update BackgroundGenerator.java
    Update:
    
    Broken up into several classes
    Addition of privacy settings - Paint class becomes draw methods in Shape.java
    Removal of unused comments
    Multiple outputs commands broken down into several methods.
    Extension of Canvas was removed.
    
    Code removed for reference: 2020. [online] Available at: <https://stackoverflow.com/questions/74674/how-do-i-check-cpu-and-memory-usage-in-java //https://refactoring.guru/design-patterns/flyweight/java/example> [Accessed 24 November 2020].

--- src/BackgroundGenerator.java
+++ src/BackgroundGenerator.java
@@ -1,140 +1,100 @@
-import javax.swing.*;
+package generator;
+
+import shape.ShapeAdder;
 import java.awt.*;
-import java.awt.Shape;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.NumberFormat;
 
 
-public class BackgroundGenerator extends Canvas implements ActionListener {
-    //boolean waiting;
-    private int x;
-    private int y;
-    String name;
-    Color color;
-    int height;
-    int width;
-    int guiWidth = 600;
-    int guiHeight = 500;
+ class BackgroundGenerator implements ActionListener {
+    private int guiWidth = 600;
+    private int guiHeight = 500;
+    private final int SHAPES_TO_DRAW = 500000;
+    private int shapeTypes = 4;
+    private ShapeAdder shapeAdder = new ShapeAdder();
+    private NumberFormat format = NumberFormat.getInstance();
+    private Runtime runtime = Runtime.getRuntime();
+    private long maxAmountMemory = runtime.maxMemory();
+    private long allocatedMemory = runtime.totalMemory();
+
+      void addShapes() {
+
+        int shapesOnScreen = (SHAPES_TO_DRAW / shapeTypes);
 
 
-    BackgroundGenerator() {
+          for (int i = 0; i < Math.floor(shapesOnScreen); i++) {
+              shapeAdder.addShape(random(0, guiWidth), random(0, guiHeight), "Square", Color.PINK, 10, 10);
+              shapeAdder.addShape(random(0, guiWidth), random(0, guiHeight), "Rectangle", Color.RED, 50, 20);
+              shapeAdder.addShape(random(0, guiWidth), random(0, guiHeight), "Circle", Color.YELLOW, 20, 20);
+              shapeAdder.addShape(random(0, guiWidth), random(0, guiHeight), "Small Rectangle", Color.BLUE, 5, 5);
+          }
 
-        this.x = x;
-        this.y = y;
-        this.name = name;
-        this.color = color;
-        this.height = height;
-        this.width = width;
-        //notGeneratingBackground();
+          addHeadings();
+          shapeSizes();
+          ramUsed();
+          shapeAdder();
+          timeToGenerate();
+          isMemory();
+      }
 
+       String addHeadings() {
+         return  ("Background generated." + SHAPES_TO_DRAW + " shapes drawn" +
+                            "---------------------");
+     }
+
+     String shapeSizes() {
+        return  ("---------------------" + "\nMemory usage:" +
+                           "shape size (8 bytes) * " + SHAPES_TO_DRAW +
+                           "+ ShapeTypes size (~30 bytes) * " + SHAPES_TO_DRAW + ".");
     }
 
-    @Override
-    public void paint(Graphics g) {
+      String ramUsed() {
+        return ("---------------------" + "Total: " +
+                          ((SHAPES_TO_DRAW * 8 + shapeTypes * 30) / 1024 / 1024) +
+                          "MB (instead of " + ((SHAPES_TO_DRAW * 38) / 1024 / 1024) + "MB)");
 
-        Graphics2D graphics = (Graphics2D) g;
+     }
 
-        //15 shapes
-        graphics.fillRect(random(0, guiWidth),random(0, guiHeight), 50, 20);
-        graphics.fillOval(random(0, guiWidth),random(0, guiHeight), 5, 20);
-        graphics.fillRect(random(0, guiWidth),random(0, guiHeight), 43, 13);
-        graphics.fillRect(random(0, guiWidth),random(0, guiHeight), 29, 20);
-        drawRectangle(g);
-        graphics.fillRect(random(0, guiWidth),random(0, guiHeight), 30, 20);
-        graphics.fillRect(random(0, guiWidth),random(0, guiHeight), 50, 20);
-        graphics.fillOval(random(0, guiWidth),random(0, guiHeight), 50, 20);
-        graphics.fillOval(random(0, guiWidth),random(0, guiHeight), 20, 20);
-        graphics.fillRect(random(0, guiWidth),random(0, guiHeight), 10, 20);
-        graphics.fillRect(random(0, guiWidth),random(0, guiHeight), 14, 20);
-        graphics.fillRect(random(0, guiWidth),random(0, guiHeight), 5, 20);
-        graphics.fillRect(random(0, guiWidth),random(0, guiHeight), 50, 20);
-        graphics.fillRect(random(0, guiWidth),random(0, guiHeight), 6, 20);
-        graphics.fillOval(random(0, guiWidth),random(0, guiHeight), 7, 20);
-        graphics.fillRect(random(0, guiWidth),random(0, guiHeight), 5, 20);
-
+    private void shapeAdder() {
+        shapeAdder.setSize(guiWidth, guiHeight);
+        shapeAdder.setVisible(true);
     }
 
-    public void drawRectangle(Graphics g) {
-        Graphics2D graphics = (Graphics2D) g;
-        graphics.fillRect(random(0, guiWidth),random(0, guiHeight),50,20);
+     String timeToGenerate() {
+        long startOfMemory = System.currentTimeMillis();
+        long endOfMemory = System.currentTimeMillis();
+
+        return  ("\nShapes take " + (endOfMemory - startOfMemory) + " ms to generate.");
     }
 
+    String isMemory() {
+        long freeMemory = runtime.freeMemory();
+        long totalAvailableMemory = Runtime.getRuntime().totalMemory();
+        long totalMemoryUsed = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        StringBuilder stringBuilder = new StringBuilder();
 
+        return stringBuilder + ( "\nmax memory: " + format.format(maxAmountMemory / 1024) + " mb.") + "\n"
+                               + stringBuilder + ( "\nAllocated memory: " + format.format(allocatedMemory / 1024) + " mb.") + "\n"
+                               + stringBuilder + ( "\nFree memory: " + format.format(freeMemory / 1024) + " mb.") + "\n"
+                               + "------------------------------------------------------------------------------" + "\n"
+                               + stringBuilder + ( "\nTotal free memory: " + format.format(freeMemory + (maxAmountMemory- allocatedMemory))) + " mb."
+                               + "\n" + totalAvailableMemory + " mb left in memory." + "\n"
+                               +  totalMemoryUsed + " mb left in memory." + "\n";
+    }
 
-    /*public void notGeneratingBackground() {
-        if (!waiting) {
-            System.out.println("error");
-        } else
-            JOptionPane.showMessageDialog(null, "Waiting on background generation.");
-    }*/
 
     public void actionPerformed(ActionEvent e) {
         System.exit(0);
     }
 
 
-    public static int random(int min, int max) {
+     static int random(int min, int max) {
         min = 1;
         return min + (int) (Math.random() * ((max - min) + 1));
     }
-
-    public static void main(String[] args) {
-        long start = System.currentTimeMillis();
-        BackgroundGenerator backgroundGenerator = new BackgroundGenerator();
-        JFrame jFrame = new JFrame();
-        jFrame.setSize(500,500);
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jFrame.getContentPane().add(backgroundGenerator);
-        jFrame.setVisible(true);
-
-        int SHAPES_TO_DRAW = 15;
-        int SHAPE_TYPES = 2;
-        //  waiting = false;
-
-        long end = System.currentTimeMillis();
-        System.out.println("Shapes take " +
-                (end - start) + "ms to generate.");
-
-        Runtime runtime = Runtime.getRuntime();
-
-        NumberFormat format = NumberFormat.getInstance();
-
-        StringBuilder sb = new StringBuilder();
-        long maxMemory = runtime.maxMemory();
-        long allocatedMemory = runtime.totalMemory();
-        long freeMemory = runtime.freeMemory();
-
-        //MEM USAGE
-        //RAM USAGE
-        //TIME TAKEN
-
-        //REF ME REM mehttps://stackoverflow.com/questions/74674/how-do-i-check-cpu-and-memory-usage-in-java
-        //https://refactoring.guru/design-patterns/flyweight/java/example
-        long total = Runtime.getRuntime().totalMemory();
-        long used  = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-        System.out.println(sb.append("free memory: " + format.format(freeMemory / 1024) + " mb."));
-        System.out.println(sb.append("free memory: " + format.format(freeMemory / 1024) + " mb."));
-        System.out.println(sb.append("\nallocated memory: " + format.format(allocatedMemory / 1024) + " mb."));
-        System.out.println(sb.append("\nmax memory: " + format.format(maxMemory / 1024) + " mb."));
-        System.out.println(sb.append("\ntotal free memory: " + format.format(freeMemory + (maxMemory - allocatedMemory))) + " mb.");
-        System.out.println(total + " mb available in memory.");
-        System.out.println(used + " mb available in memory.");
-        System.out.println("Background generated.");
-        System.out.println(SHAPES_TO_DRAW + " shapes drawn");
-        System.out.println("---------------------");
-        System.out.println("Memory usage:");
-        System.out.println("shape size (8 bytes) * " + SHAPES_TO_DRAW);
-        System.out.println("+ ShapeTypes size (~30 bytes) * " + SHAPES_TO_DRAW + "");
-        System.out.println("---------------------");
-        System.out.println("Total: " + ((SHAPES_TO_DRAW  * 38) / 1024 / 1024) +
-                "MB (instead of " + ((SHAPES_TO_DRAW * 8 + SHAPE_TYPES  * 30) / 1024 / 1024) + "MB)");
-
-        //jFrame.setSize(500,500);*/
-        //jFrame.setVisible(true);
-    }
-
 }
+
 
 
 
